f9aad22d75e38da30c10868fe58219928248accd
lzm420241 <lzm420241@gmail.com>
2016-03-30 22:08:54 +0800

    add back top

--- js/main.js
+++ js/main.js
@@ -14,5 +14,18 @@ $(function () {
 
   $(".qr-pic").click(function(){
   	 $(".popover").toggle();
+  });
+  if(document.body.scrollTop == 0){
+  		$(".back-top").hide();
+  	}else{
+  		$(".back-top").show();
+  	}
+   $(window).scroll(function(){
+  	if(document.body.scrollTop == 0){
+  		$(".back-top").hide();
+  	}else{
+  		$(".back-top").show();
+  	}
   });	
+  
 })
