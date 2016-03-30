dc9d6f21bde1fa525e3434c275762d4776bf9507
lzm420241 <lzm420241@gmail.com>
2016-03-30 21:11:44 +0800

    change qr animation

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
-$(function(){
-	$(".show-qr").click(function(){
-		$(".qr").animate({right:"10px"});
-		$(this).hide();
-	});
-	$(".qr").click(function(){
-		$(this).animate({right:"-100px"});
-		$(".show-qr").show();
-	});
+// $(function(){
+// 	$(".show-qr").click(function(){
+// 		$(".qr").animate({right:"10px"});
+// 		$(this).hide();
+// 	});
+// 	$(".qr").click(function(){
+// 		$(this).animate({right:"-100px"});
+// 		$(".show-qr").show();
+// 	});
+// })
+
+$(function () {
+  $('[data-toggle="popover"]').popover();
+
+  // $('[data-toggle="popover"]').on('show.bs.popover',function(){
+  // 	$(".popover-content").append('<img src="images/qr.jpg">');
+  // 	alert("hello");
+  // });
 })
